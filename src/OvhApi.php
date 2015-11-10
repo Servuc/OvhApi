e711e0063f62486acfc1e48ccf96cdde3ed11930
Thomas Servuc <thomas.selle@orange.fr>
2015-11-10 01:06:22 +0100

    ... no comment, git add forget

---
+++ src/OvhApi.php
@@ -0,0 +1,158 @@
+<?php
+
+class OvhApi
+{
+    //FROM : https://github.com/ovh/php-ovh
+    public static $roots = array(
+       'ovh-eu' => 'https://eu.api.ovh.com/1.0',
+       'ovh-ca' => 'https://ca.api.ovh.com/1.0',
+       'kimsufi-eu' => 'https://eu.api.kimsufi.com/1.0',
+       'kimsufi-ca' => 'https://ca.api.kimsufi.com/1.0',
+       'soyoustart-eu' => 'https://eu.api.soyoustart.com/1.0',
+       'soyoustart-ca' => 'https://ca.api.soyoustart.com/1.0',
+       'runabove-ca' => 'https://api.runabove.com/1.0');
+
+    protected $AK;
+    protected $AS;
+    protected $CK;
+    protected $serviceName;
+    protected $timeDrift = 0;
+    protected $cliVersion;
+
+    /**
+     * constructor
+     * @param $_root API service URL (give in $roots array)
+     * @param $_ak Your app key
+     * @param $_as Your app secret key
+     * @param $_cliVersion Set it to false if you're in a web page
+     */
+    public function __construct($_root, $_ak, $_as, $_cliVersion = true)
+    {
+    	if($_cliVersion && is_file("CK"))
+    	{
+			$myFichier = fopen('CK', 'r+');
+			$this->CK = substr(fgets($myFichier), 0, -1);
+			fclose($myFichier);
+		}
+        else {
+            if( ! isset($_SESSION))
+                throw new Exception("Add session_start() on your script !");
+
+            $this->CK = $_SESSION["ck"];
+        }
+
+        // INIT vars
+        $this->AK = $_ak;
+        $this->AS = $_as;
+
+        $this->ROOT = $_root;
+        $this->cliVersion = $_cliVersion;
+
+        // Compute time drift
+        $serverTimeRequest = file_get_contents($this->ROOT . '/auth/time');
+        if($serverTimeRequest !== FALSE)
+            $this->timeDrift = time() - (int)$serverTimeRequest;
+        else
+            die('ERROR (#0000) : Compute time drift fail !\n');
+    }
+
+    /**
+     * Call the api
+     * @param $_method GET POST DELETE or UPDATE
+     * @param $_url The call url wanted
+     * @param $_body Parameters
+     */
+    public function call($_method, $_url, $_body = "")
+    {
+        $myUrl = $this->ROOT . $_url;
+        if($_body != "")
+            $_body = json_encode($_body);
+
+        // Compute signature
+        $time = time() - $this->timeDrift;
+        $toSign = $this->AS.'+'.$this->CK.'+'.$_method.'+'.$myUrl.'+'.$_body.'+'.$time;
+        
+        $signature = '$1$' . sha1($toSign);
+
+        // Call
+        $curl = curl_init($myUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $_method);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type:application/json',
+            'X-Ovh-Application:' . $this->AK,
+            'X-Ovh-Consumer:' . $this->CK,
+            'X-Ovh-Signature:' . $signature,
+            'X-Ovh-Timestamp:' . $time,
+        ));
+        if($_body)
+        {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $_body);
+        }
+        $result = curl_exec($curl);
+
+        if($result === FALSE)
+        {
+            echo curl_error($curl);
+            return NULL;
+        }
+
+        return json_decode($result, true);
+    }
+
+    public function get($url)
+    {
+        return $this->call("GET", $url);
+    }
+
+    public function put($url, $body)
+    {
+        return $this->call("PUT", $url, $body);
+    }
+
+    public function post($url, $body)
+    {
+        return $this->call("POST", $url, $body);
+    }
+
+    public function delete($url)
+    {
+        return $this->call("DELETE", $url);
+    }
+
+    /**
+     * Permit you to ask credential to allow ask on api
+     * @param $_ak Your application key
+     * @param $_root API service URL (give in $roots array)
+     * @param $_cliVersion Set it to false if you're in a web page. Think to add session_start();
+     */
+    public static function getCredential($_ak, $_root, $_access, $_cliVersion = true)
+    {
+    	$curl = curl_init($_root . "/auth/credential");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type:application/json',
+            'X-Ovh-Application:' . $_ak
+        ));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $_access);
+        $result = curl_exec($curl);
+        echo "Voici votre consumerKey : ";
+    	echo preg_replace("/^.*(\"consumerKey\":\")([A-Za-z0-9]+).*$/" , "$2" , $result);
+    	echo "\nVoici l'url de validation pour activer votre compte : ";
+    	echo "https://api.ovh.com/auth/?credentialToken=" . preg_replace("/^.*(credentialToken=)([A-Za-z0-9]+).*$/" , "$2" , $result);
+    	echo "\n";
+
+        if($_cliVersion)
+    	   system("echo " . preg_replace("/^.*(\"consumerKey\":\")([A-Za-z0-9]+).*$/" , "$2" , $result) . " > CK");
+        else  {
+            if( ! isset($_SESSION))
+                throw new Exception("Add session_start() on your script !");
+
+            $_SESSION["ck"] = preg_replace("/^.*(\"consumerKey\":\")([A-Za-z0-9]+).*$/" , "$2" , $result);
+        }
+
+    }
+}
+
+?>
---
+++ src/cliAskDomains.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include "OvhApi.php";
+
+$AK = "***********************";
+$AS = "***********************";
+
+if($argc >= 2)
+{
+	switch ($argv[1])
+	{
+		case "-g" :
+			//You MUST do this the first time
+            $myAccess = '{"accessRules": [{"method": "GET","path": "/*"}]}';
+			OvhApi::getCredential($AK, OvhApi::$roots["ovh-eu"], $myAccess);
+		break;
+
+		case "-d" :
+        	$mVarOvh = new OvhApi(OvhApi::$roots["ovh-eu"], $AK, $AS);
+			print_r($mVarOvh->get("/domain"));
+		break;
+	}
+
+    echo "\n";
+}
+else
+    echo "Usage : " . $argv[0] . " option\n";
+
+?>
